1d3fff06c173f4856e2b3d5b7c166c12936bea71
Joel.Dun <joel.dun@pm.me>
2023-01-13 09:20:10 -0500

    Create playRound function.

--- javascript.js
+++ javascript.js
@@ -3,26 +3,33 @@ function getComputerChoice ([input]) {
   return computerSelection
 }
 
+
+
+
+function playRound(playerSelection, computerSelection) {
+  if (playerSelection == "rock" && computerSelection == "paper"){
+    return "You lose! Paper beats rock!"
+  }
+  if (playerSelection == "paper" && computerSelection == "scissors"){
+    return "You lose! Scissors beats paper!"
+  }
+  if (playerSelection == "scissors" && computerSelection == "rock"){
+    return "You lose! Rock beats scissors!"
+  }
+
+  if (playerSelection == "rock" && computerSelection == "scissors"){
+    return "You win! Rock beats scissors!"
+  }
+  if (playerSelection == "paper" && computerSelection == "rock"){
+    return "You win! Paper beats rock!"
+  }
+  if (playerSelection == "scissors" && computerSelection == "paper"){
+    return "You win! Scissors beats paper!"
+  }
+}
+ 
+const playerSelection = "rock";
+const computerSelection = getComputerChoice();
 const possibleChoices = ["Rock", "Paper", "Scissors"];
 
-
-
-
-
-console.log(getComputerChoice(possibleChoices))
-
-
-
-
-
-
-
-
-
-// function playRound(playerSelection, computerSelection) {
- 
-// }
- 
-// const playerSelection = "rock";
-// const computerSelection = getComputerChoice();
-// console.log(playRound(playerSelection, computerSelection));
+console.log(playRound(playerSelection, computerSelection));
