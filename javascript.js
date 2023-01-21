bf1f783706d727a5992d06417fde896c1eff0155
Joel Dunnington <joeldunnington@Joels-MacBook-Pro.local>
2023-01-21 11:42:56 -0600

    Fix typo.

--- javascript.js
+++ javascript.js
@@ -1,39 +1,48 @@
 
-function getComputerChoice (input) {
+function getComputerChoice () {
   let computerSelection = possibleChoices[Math.floor(Math.random() * possibleChoices.length)];
   return computerSelection
 }
 
+const possibleChoices = ["rock", "paper", "scissors"];
 
-
-
-function playRound(playerSelection, computerSelection) {
-  if (playerSelection == "rock" && computerSelection == "paper"){
+function playRound (playerSelection, computerSelection) {
+  if (playerSelection == "rock" && computerSelection == "paper") {
     return "You lose! Paper beats rock!"
   }
-  if (playerSelection == "paper" && computerSelection == "scissors"){
+  if (playerSelection == "paper" && computerSelection == "scissors") {
     return "You lose! Scissors beats paper!"
-  }
-  if (playerSelection == "scissors" && computerSelection == "rock"){
+   }
+  if (playerSelection == "scissors" && computerSelection == "rock" ) {
     return "You lose! Rock beats scissors!"
   }
-
-  if (playerSelection == "rock" && computerSelection == "scissors"){
+  if (playerSelection == "rock" && computerSelection == "scissors") {
     return "You win! Rock beats scissors!"
   }
-  if (playerSelection == "paper" && computerSelection == "rock"){
+  if (playerSelection == "paper" && computerSelection == "rock") {
     return "You win! Paper beats rock!"
   }
-  if (playerSelection == "scissors" && computerSelection == "paper"){
+  if (playerSelection == "scissors" && computerSelection == "paper") {
     return "You win! Scissors beats paper!"
+  }
+  else {
+    return "Draw!"
   }
 }
 
-const possibleChoices = ["rock", "paper", "scissors"];
+function game () {
+ let winCount = 0;
+ let loseCount = 0;
+ 
+  for (let i = 0; i < 5; i++) {
+    let playerSelection = "rock";
+    let computerSelection = getComputerChoice;
+    console.log(getComputerChoice())
 
-const playerSelection = "rock";
-const computerSelection = getComputerChoice();
+    let result = playRound(playerSelection, computerSelection())
+    return result
+  
+  }
+}
 
-console.log(playerSelection)
-console.log(computerSelection)
-console.log(playRound(playerSelection, computerSelection));
+console.log(game())
