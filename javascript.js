e27f1fb27e097e84c53e29c536e9df2699b1d7b1
Joel Dunnington <joeldunnington@Joels-MacBook-Pro.local>
2023-01-25 12:31:57 -0600

    Playround returns results with score

--- javascript.js
+++ javascript.js
@@ -4,43 +4,57 @@ function getComputerChoice () {
   return computerSelection
 }
 
+let winCount = 0;
+let loseCount = 0;
+let drawCount = 0;
+
 const possibleChoices = ["rock", "paper", "scissors"];
 
 function playRound (playerSelection, computerSelection) {
   if (playerSelection == "rock" && computerSelection == "paper") {
-    return "You lose! Paper beats rock!"
+    loseCount++
+    return "You lose! Paper beats rock!" 
   }
   if (playerSelection == "paper" && computerSelection == "scissors") {
+    loseCount++
     return "You lose! Scissors beats paper!"
    }
   if (playerSelection == "scissors" && computerSelection == "rock" ) {
+    loseCount++
     return "You lose! Rock beats scissors!"
   }
   if (playerSelection == "rock" && computerSelection == "scissors") {
+    winCount++
     return "You win! Rock beats scissors!"
   }
   if (playerSelection == "paper" && computerSelection == "rock") {
+    winCount++
     return "You win! Paper beats rock!"
   }
   if (playerSelection == "scissors" && computerSelection == "paper") {
+    winCount++
     return "You win! Scissors beats paper!"
   }
   else {
+    drawCount++
     return "Draw!"
   }
 }
 
 function game () {
- let winCount = 0;
- let loseCount = 0;
- 
+  let result = ""
   for (let i = 0; i < 5; i++) {
     let playerSelection = "rock";
     let computerSelection = getComputerChoice;
-    let result = playRound(playerSelection, computerSelection(i))
-    console.log(result)
+    result = playRound(playerSelection, computerSelection(i))
   }
-  
+  if (winCount > loseCount) {
+    return "You win with a score of " + winCount + " to " + loseCount + "!"
+  }
+  else {
+    return "You lose with a score of " + loseCount + " to " + winCount + "!"
+  }
+
 }
 
 console.log(game())
